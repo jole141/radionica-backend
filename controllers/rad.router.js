7e1f6a6f521491964eccad79bd53cf0494752c21
Josip Jurenić <josip.jurenic12@gmail.com>
2023-04-29 09:03:51 +0200

    add endpoint for dijelovi

--- controllers/rad.router.js
+++ controllers/rad.router.js
@@ -1,11 +1,45 @@
 const { Router } = require("express");
-const { getDijelovi } = require("../datasource");
+const {
+  getDijelovi,
+  postDio,
+  deleteDio,
+  putDio,
+  getDio,
+} = require("../datasource");
 
 const radRouter = Router();
 
 radRouter.get("/dijelovi", async (req, res, next) => {
   const dijelovi = await getDijelovi();
   res.status(200).json(dijelovi);
+});
+
+radRouter.post("/dijelovi", async (req, res, next) => {
+  const dio = req.body;
+  await postDio(dio);
+  res.status(201).json({ message: "dio added" });
+});
+
+radRouter.delete("/dijelovi/:id", async (req, res, next) => {
+  const id = req.params.id;
+  await deleteDio(id);
+  res.status(200).json({ message: "dio deleted" });
+});
+
+radRouter.put("/dijelovi/:id", async (req, res, next) => {
+  const dio = req.body;
+  const id = req.params.id;
+  await putDio(dio, id);
+  res.status(200).json({ message: "dio updated" });
+});
+
+radRouter.get("/dijelovi/:id", async (req, res, next) => {
+  const id = req.params.id;
+  const dio = await getDio(id);
+  if (!dio) {
+    return res.status(404).json({ error: "dio not found" });
+  }
+  res.status(200).json(dio);
 });
 
 module.exports = { radRouter };
--- datasource.js
+++ datasource.js
@@ -17,4 +17,30 @@ const getDijelovi = async () => {
   return results.rows;
 };
 
-module.exports = { getDijelovi };
+const postDio = async (dio) => {
+  await pool.query(
+    "INSERT INTO dio (naziv_dijela, kolicina_na_lageru) VALUES ($1, $2);",
+    [dio.nazivDio, dio.kolicinaNaLageru]
+  );
+};
+
+const deleteDio = async (id) => {
+  await pool.query("DELETE FROM dio WHERE sifra_dijela = $1;", [id]);
+};
+
+const putDio = async (dio, id) => {
+  await pool.query(
+    "UPDATE dio SET naziv_dijela = $1, kolicina_na_lageru = $2 WHERE sifra_dijela = $3;",
+    [dio.nazivDio, dio.kolicinaNaLageru, id]
+  );
+};
+
+const getDio = async (id) => {
+  const results = await pool.query(
+    "SELECT * from dio WHERE sifra_dijela = $1;",
+    [id]
+  );
+  return results.rows[0];
+};
+
+module.exports = { getDijelovi, postDio, deleteDio, putDio, getDio };
